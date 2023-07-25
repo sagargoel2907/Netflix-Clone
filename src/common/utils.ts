ddb6be9f9c178aa215c4aecc926cf435883b36a9
sagar_goel <sagar_goel@hcl.com>
2023-07-25 23:42:39 +0530

    added orginal images

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,4 +1,5 @@
 export function getImageURL(poster_path: string, width: number) {
-  console.log(`${import.meta.env.VITE_BASE_IMAGE_URL}${poster_path}`);
-  return `${import.meta.env.VITE_BASE_IMAGE_URL}/w${width}${poster_path}`;
+  const url = `${import.meta.env.VITE_BASE_IMAGE_URL}${poster_path}`;
+  console.log(url);
+  return url;
 }
